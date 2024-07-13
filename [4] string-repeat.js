4af6592b313d3f12df8b37595cbc511e439d40c3
Ahmed Maher <ahmedmaheraljwhry057@gmail.com>
2024-07-14 00:21:39 +0300

    Add a new challenge: Convert number to reversed array of digits

--- [4] string-repeat.js
+++ [4] string-repeat.js
@@ -1,5 +1,6 @@
 // Challenge Link: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
 
+
 // Solution Method_1 [using a loop]:
 function repeatStr (n, s) {
     let totalString = '';
---
+++ [5] convert-number-to-reversed-array-of-digits.js
@@ -0,0 +1,16 @@
+// Challenge Link: https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+
+
+// Solution:
+function digitize(number) {
+    // Convert the number to array of characters
+    let digitArray = number.toString().split('');
+
+    // Convert each character to a number then Reverse the array and return it
+    return digitArray.map(str => Number(str)).reverse();
+}
+
+
+// Test Cases:
+console.log(digitize(35231));             // Output: [1,3,2,5,3]
+console.log(digitize(0));                 // Output: [0] 
